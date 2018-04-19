b739797dfc7108bea2509653eaa160c52175f270
Francisco Alonzo <franalonzo96@gmail.com>
2018-04-18 21:03:54 -0600

    Convertir Usuarios a Json y de Json objetos

--- Proyecto_ED1/Proyecto_ED1/DBContext/JsonFile.cs
+++ Proyecto_ED1/Proyecto_ED1/DBContext/JsonFile.cs
@@ -30,5 +30,45 @@ namespace Proyecto_ED1.DBContext
 
         }
 
+        public void UserToJson(List<Usuario> user)
+        {
+            try
+            {
+
+                using (StreamWriter file = File.CreateText(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\Usuarios.json"))
+                {
+                    var serializer = new JsonSerializer();
+                    serializer.Serialize(file, user);
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public List<Usuario> UsersToList(Stream Path)
+        {
+            List<Usuario> array;
+            try
+            {
+                using (var reader1 = new StreamReader(Path))
+                {
+                    var lecture = reader1.ReadToEnd();
+                    array = JsonConvert.DeserializeObject<List<Usuario>>(lecture);
+                    reader1.Close();
+                }
+                return array;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+
+            }
+        }
+
+
     }
 }
